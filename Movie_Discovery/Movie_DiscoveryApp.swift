6ae12029e672c3d2d8719a2aa50b767628dcc33d
Michel Chu <chu.michel@yahoo.fr>
2022-04-04 15:41:20 +0200

    Initial Commit

---
+++ Movie_Discovery/Movie_DiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_DiscoveryApp.swift
+//  Movie_Discovery
+//
+//  Created by Square on 04/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Movie_DiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
